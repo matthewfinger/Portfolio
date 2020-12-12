24f1fad9043b0b30ebe0b89ac2c366f39c151b3f
matthewfinger <matthewfinger3@gmail.com>
2020-12-12 08:36:07 -0600

    12/12/2020 : tweaked pagecontend stylin

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -10,9 +10,9 @@ class Footer extends BaseComponent {
     const linkedinUrl = this.props.linkedinUrl || 'https://www.linkedin.com/in/matthew-finger-1b5766164?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BZmuEzprVTL25ZLP8TKxNyg%3D%3D';
     return (
       <div>
-        <div><a href={emailUrl} target='_blank'><img src={emailIconSrc} alt="email"/></a></div>
-        <div><a href={phoneUrl} target='_blank'><img src={phoneIconSrc} alt="call me"/></a></div>
-        <div><a href={linkedinUrl} target='_blank'><img src={linkedinIconSrc} alt="email"/></a></div>
+        <div><a href={emailUrl} target='_blank' rel='noreferrer'><img src={emailIconSrc} alt="email"/></a></div>
+        <div><a href={phoneUrl} target='_blank' rel='noreferrer'><img src={phoneIconSrc} alt="call me"/></a></div>
+        <div><a href={linkedinUrl} target='_blank' rel='noreferrer'><img src={linkedinIconSrc} alt="email"/></a></div>
       </div>
     );
   }
