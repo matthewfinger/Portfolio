d039646e34e8c7c4d73d0e2080c69ccf5075f607
Matthew Finger <52319793+matthewfinger@users.noreply.github.com>
2021-11-10 21:31:43 -0600

    now it should work

--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -65,7 +65,7 @@ class MainContent extends Content {
         </div>
         <ImageComponent functions={this.state.functions} containerId="section1LogoContainer" id='section1Logo' imageName="Logo1" />
       </section>
-      <br style={{height: '5vh'}}/>
+      <br style={{height: '5vh', width: '100%', display: 'block'}}/>
       <br/>
       </>
     );
