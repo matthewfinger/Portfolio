a5e8426c9704f44f23457a4ce42a759659864d52
Matt Finger <matthewfinger3@gmail.com>
2022-06-19 09:31:17 -0500

    Use var for command

--- sync.sh
+++ sync.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 basedir=/home/matthewfinger/website/Portfolio
-alias permissionfix="chmod -R ugo+rwx $basedir"
+permissionfix="chmod -R ugo+rwx $basedir"
 
 cd $basedir
 git pull origin main
 
 cd $basedir/frontend
-permissionfix
+$permissionfix
 su - matthewfinger -c "cd $basedir/frontend; npm update"
 npm run build
-permissionfix
+$permissionfix
 
 cd $basedir
 su - matthewfinger -c "cd $basedir;pipenv run python $basedir/backend/manage.py makemigrations"
@@ -20,7 +20,7 @@ apache2ctl restart
 
 #lastly push since we might have migrations chagnes
 cd $basedir
-permissionfix
+$permissionfix
 
 su - matthewfinger -c "cd $basedir && git add ."
 su - matthewfinger -c "cd $basedir && git commit -m\"Built on cloud\""
